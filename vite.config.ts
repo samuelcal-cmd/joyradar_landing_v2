35f4a144e207f798f89c461bdba07ad06f65b118
samuelcal-cmd <samuel.cal@mpxa.net>
2026-02-01 23:46:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Un-comment the line below and replace 'REPO_NAME' with your actual GitHub repository name
   // if you are deploying to GitHub Pages at username.github.io/REPO_NAME
-  // base: '/joyradar/', 
+  base: '/joyradar_landing_v2/', 
 });
